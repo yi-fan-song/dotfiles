ac8b081696df0c8290f38fcc66639c333f826138
宋逸凡 <44707828+yi-fan-song@users.noreply.github.com>
2024-05-27 20:12:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 
 # Install latex deps
-apt-get update
-apt-get install -y libyaml-tiny-perl libfile-homedir-perl
+sudo apt-get update
+sudo apt-get install -y libyaml-tiny-perl libfile-homedir-perl
 
 # Install Latex
 cd /tmp # working directory of your choice
 curl -L -o install-tl-unx.tar.gz https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat < install-tl-unx.tar.gz | tar xf -
 cd install-tl-2*
-perl ./install-tl --no-interaction # as root or with writable destination
+sudo perl ./install-tl --no-interaction # as root or with writable destination
 echo 'export PATH="/usr/local/texlive/2024/bin/x86_64-linux:$PATH"' >> ~/.bash_aliases
